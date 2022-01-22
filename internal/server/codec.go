714b464d31f4c4d2b1220686206a94d8b22385fc
Kevin Wang <kevmo314@gmail.com>
2022-01-22 15:21:57 -0500

    update

--- internal/server/codec.go
+++ internal/server/codec.go
@@ -7,35 +7,104 @@ import (
 	"github.com/pion/webrtc/v3"
 )
 
+// mirroring https://chromium.googlesource.com/external/webrtc/+/95eb1ba0db79d8fd134ae61b0a24648598684e8a/webrtc/media/engine/payload_type_mapper.cc#27
+//
+// TODO: a better approach to this would be to list all supported codecs (including duplicates) and let WebRTC negotiate the codec
+// then pass that to the transcoder. we will need to implement our own TrackLocal for this.
 var DefaultOutputCodecs = map[string]webrtc.RTPCodecParameters{
-	webrtc.MimeTypeH264: {
-		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeH264, ClockRate: 90000},
-		PayloadType: 96,
+	webrtc.MimeTypePCMU: {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypePCMU, ClockRate: 8000, Channels: 1},
+		PayloadType: 0,
 	},
-	webrtc.MimeTypeH265: {
-		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeH265, ClockRate: 90000},
-		PayloadType: 98,
+	"audio/GSM": {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/GSM", ClockRate: 8000, Channels: 1},
+		PayloadType: 3,
 	},
+	"audio/G723": {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/G723", ClockRate: 8000, Channels: 1},
+		PayloadType: 4,
+	},
+	"audio/LPC": {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/LPC", ClockRate: 8000, Channels: 1},
+		PayloadType: 7,
+	},
+	webrtc.MimeTypePCMA: {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypePCMA, ClockRate: 8000, Channels: 1},
+		PayloadType: 8,
+	},
+	webrtc.MimeTypeG722: {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeG722, ClockRate: 8000, Channels: 1},
+		PayloadType: 9,
+	},
+	"audio/L16": {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/L16", ClockRate: 44100, Channels: 2},
+		PayloadType: 10,
+	},
+	"audio/QCELP": {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/QCELP", ClockRate: 8000, Channels: 1},
+		PayloadType: 12,
+	},
+	"audio/CN": {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/CN", ClockRate: 8000, Channels: 1},
+		PayloadType: 13,
+	},
+	"audio/MPA": {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/MPA", ClockRate: 90000, Channels: 1},
+		PayloadType: 14,
+	},
+	"audio/G728": {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/G728", ClockRate: 8000, Channels: 1},
+		PayloadType: 15,
+	},
+	"audio/DVI4": {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/DVI4", ClockRate: 22050, Channels: 1},
+		PayloadType: 17,
+	},
+	"audio/G729": {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/G729", ClockRate: 8000, Channels: 1},
+		PayloadType: 18,
+	},
+
 	webrtc.MimeTypeVP8: {
 		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeVP8, ClockRate: 90000},
-		PayloadType: 97,
+		PayloadType: 100,
 	},
 	webrtc.MimeTypeVP9: {
 		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeVP9, ClockRate: 90000},
-		PayloadType: 99,
+		PayloadType: 101,
+	},
+	webrtc.MimeTypeH264: {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeH264, ClockRate: 90000},
+		PayloadType: 102,
+	},
+	webrtc.MimeTypeH265: {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeH265, ClockRate: 90000},
+		PayloadType: 103,
 	},
 	webrtc.MimeTypeAV1: {
 		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeAV1, ClockRate: 90000},
-		PayloadType: 100,
+		PayloadType: 104,
 	},
 
 	webrtc.MimeTypeOpus: {
 		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeOpus, ClockRate: 48000, Channels: 2},
-		PayloadType: 101,
+		PayloadType: 111,
 	},
-	webrtc.MimeTypeG722: {
-		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeOpus, ClockRate: 8000, Channels: 1},
-		PayloadType: 9,
+	"audio/AC3": {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/AC3", ClockRate: 48000, Channels: 1},
+		PayloadType: 112,
+	},
+	"audio/VORBIS": {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/VORBIS", ClockRate: 90000, Channels: 1},
+		PayloadType: 113,
+	},
+	"audio/AAC": {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/AAC", ClockRate: 48000, Channels: 2},
+		PayloadType: 114,
+	},
+	"audio/SPEEX": {
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/SPEEX", ClockRate: 48000, Channels: 1},
+		PayloadType: 115,
 	},
 }
 type GStreamerParameters struct {
@@ -48,19 +117,19 @@ type CodecMapping struct {
 
 var SupportedCodecs = map[string]GStreamerParameters{
 	webrtc.MimeTypeH264: {
-		"rtph264depay", "x264enc speed-preset=ultrafast tune=zerolatency key-int-max=20", "rtph264pay",
+		"rtph264depay", "video/x-raw,format=I420 ! x264enc speed-preset=ultrafast tune=zerolatency key-int-max=20", "rtph264pay",
 		func(c webrtc.RTPCodecParameters) string {
-			return fmt.Sprintf("encoding-name=H264,clock-rate=%d,payload=%d", c.ClockRate, c.PayloadType)
+			return fmt.Sprintf("encoding-name=H264,clock-rate=%d,payload=%d,packetization-mode=(string)1,profile-level-id=(string)42c028", c.ClockRate, c.PayloadType)
 		},
 	},
 	webrtc.MimeTypeH265: {
-		"rtph265depay", "x265enc speed-preset=ultrafast tune=zerolatency key-int-max=20", "rtph265pay",
+		"rtph265depay", "video/x-raw,format=I420 ! x265enc speed-preset=ultrafast tune=zerolatency key-int-max=20", "rtph265pay",
 		func(c webrtc.RTPCodecParameters) string {
 			return fmt.Sprintf("encoding-name=H265,clock-rate=%d,payload=%d", c.ClockRate, c.PayloadType)
 		},
 	},
 	webrtc.MimeTypeVP8: {
-		"rtpvp8depay", "vp8enc deadline=1", "rtpvp8pay",
+		"rtpvp8depay", "vp8enc", "rtpvp8pay",
 		func(c webrtc.RTPCodecParameters) string {
 			return fmt.Sprintf("encoding-name=VP8,clock-rate=%d,payload=%d", c.ClockRate, c.PayloadType)
 		},
@@ -71,31 +140,25 @@ var SupportedCodecs = map[string]GStreamerParameters{
 			return fmt.Sprintf("encoding-name=VP9,clock-rate=%d,payload=%d", c.ClockRate, c.PayloadType)
 		},
 	},
-	webrtc.MimeTypeAV1: {
-		"rtpav1depay", "av1enc deadline=1", "rtpav1pay",
-		func(c webrtc.RTPCodecParameters) string {
-			return fmt.Sprintf("encoding-name=AV1,clock-rate=%d,payload=%d", c.ClockRate, c.PayloadType)
-		},
-	},
+	// webrtc.MimeTypeAV1: {
+	// 	"rtpav1depay", "av1enc deadline=1", "rtpav1pay",
+	// 	func(c webrtc.RTPCodecParameters) string {
+	// 		return fmt.Sprintf("encoding-name=AV1,clock-rate=%d,payload=%d", c.ClockRate, c.PayloadType)
+	// 	},
+	// },
 
 	webrtc.MimeTypeOpus: {"rtpopusdepay", "opusenc inband-fec=true", "rtpopuspay",
 		func(c webrtc.RTPCodecParameters) string {
 			return fmt.Sprintf("encoding-name=OPUS,clock-rate=%d,payload=%d", c.ClockRate, c.PayloadType)
 		},
 	},
-	"audio/aac": {
+	"audio/AAC": {
 		"rtpmp4adepay", "avenc_aac", "rtpmp4apay",
 		func(c webrtc.RTPCodecParameters) string {
-			return fmt.Sprintf("encoding-name=MP4A-LATM,clock-rate=%d,payload=%d", c.ClockRate, c.PayloadType)
+			return fmt.Sprintf("encoding-name=MP4A-LATM,clock-rate=%d,payload=%d,cpresent=(string)0,config=(string)40002320", c.ClockRate, c.PayloadType)
 		},
 	},
-	"audio/mpeg": {
-		"rtpmpadepay", "lamemp3enc", "rtpmpapay",
-		func(c webrtc.RTPCodecParameters) string {
-			return fmt.Sprintf("encoding-name=MPA,clock-rate=%d,payload=%d", c.ClockRate, c.PayloadType)
-		},
-	},
-	"audio/speex": {
+	"audio/SPEEX": {
 		"rtpspeexdepay", "speexenc", "rtpspeexpay",
 		func(c webrtc.RTPCodecParameters) string {
 			return fmt.Sprintf("encoding-name=SPEEX,clock-rate=%d,payload=%d", c.ClockRate, c.PayloadType)
@@ -104,7 +167,7 @@ var SupportedCodecs = map[string]GStreamerParameters{
 	webrtc.MimeTypeG722: {
 		"rtpg722depay", "avenc_g722", "rtpg722pay",
 		func(c webrtc.RTPCodecParameters) string {
-			return fmt.Sprintf("encoding-name=G722,clock-rate=%d,payload=%d", c.ClockRate, c.PayloadType)
+			return fmt.Sprintf("clock-rate=%d,payload=%d", c.ClockRate, c.PayloadType)
 		},
 	},
 	webrtc.MimeTypePCMA: {
@@ -119,16 +182,10 @@ var SupportedCodecs = map[string]GStreamerParameters{
 			return fmt.Sprintf("encoding-name=PCMU,clock-rate=%d,payload=%d", c.ClockRate, c.PayloadType)
 		},
 	},
-	"audio/ac3": {
+	"audio/AC3": {
 		"rtpac3depay", "avenc_ac3", "rtpac3pay",
 		func(c webrtc.RTPCodecParameters) string {
 			return fmt.Sprintf("encoding-name=AC3,clock-rate=%d,payload=%d", c.ClockRate, c.PayloadType)
-		},
-	},
-	"audio/vorbis": {
-		"rtpvorbisdepay", "vorbisenc", "rtpvorbispay",
-		func(c webrtc.RTPCodecParameters) string {
-			return fmt.Sprintf("encoding-name=VORBIS,clock-rate=%d,payload=%d", c.ClockRate, c.PayloadType)
 		},
 	},
 }
@@ -148,19 +205,17 @@ func PipelineString(from, to webrtc.RTPCodecParameters, encoder string) (string,
 	}
 	
 	if strings.HasPrefix(from.MimeType, "video") {
-		inputCaps := fmt.Sprintf("application/x-rtp,media=(string)audio,%s", fromParameters.ToCaps(from))
-		// outputCaps := fmt.Sprintf("application/x-rtp,media=(string)video,encoding-name=(string)%s,clock-rate=(int)%d", toParameters.EncodingName, to.ClockRate)
+		inputCaps := fmt.Sprintf("application/x-rtp,media=(string)video,%s", fromParameters.ToCaps(from))
 
 		return fmt.Sprintf(
-			"appsrc format=time name=source ! %s ! rtpjitterbuffer ! %s ! queue ! decodebin ! queue ! videoconvert ! %s ! %s ! appsink name=sink sync=false async=false",
-			inputCaps, fromParameters.Depayloader, encoder, toParameters.Payloader), nil
+			"appsrc format=time is-live=true name=source ! %s ! %s ! queue ! decodebin ! autovideosink",
+			inputCaps, fromParameters.Depayloader), nil
 	} else if strings.HasPrefix(from.MimeType, "audio") {
 		inputCaps := fmt.Sprintf("application/x-rtp,media=(string)audio,%s", fromParameters.ToCaps(from))
-		// outputCaps := fmt.Sprintf("application/x-rtp,media=(string)audio,encoding-name=(string)%s,clock-rate=(int)%d", toParameters.EncodingName, to.ClockRate)
 
 		return fmt.Sprintf(
-			"appsrc format=time is-live=true name=source ! %s ! rtpjitterbuffer ! %s ! queue ! decodebin ! queue ! audioconvert ! audioresample ! %s ! %s ! appsink name=sink sync=false async=false",
-			inputCaps, fromParameters.Depayloader, toParameters.DefaultEncoder, toParameters.Payloader), nil
+			"appsrc format=time is-live=true name=source ! %s ! %s ! queue ! decodebin ! queue ! audioconvert ! audioresample ! %s ! %s mtu=1200 ! appsink name=sink sync=false async=false",
+			inputCaps, fromParameters.Depayloader, encoder, toParameters.Payloader), nil
 	}
 	return "", fmt.Errorf("unsupported codec %s", from.MimeType)
 }
--- test/e2e_test.go
+++ test/e2e_test.go
@@ -48,97 +48,32 @@ func reader(s string) (*gst.Element, error) {
 	return bin.GetByName("sink"), nil
 }
 
-// func TestTranscodingVP8ToAny(t *testing.T) {
-// 	var wg sync.WaitGroup
-// 	for mime, codec := range server.SupportedCodecs {
-// 		if !strings.HasPrefix(mime, "video") {
-// 			continue
-// 		}
-
-// 		tc, err := transcode.NewTranscoder(webrtc.RTPCodecCapability{
-// 			MimeType:  webrtc.MimeTypeVP8,
-// 			ClockRate: 90000,
-// 		}, transcode.ToMimeType(mime))
-// 		if err != nil {
-// 			t.Errorf("failed to create transcoder: %v", err)
-// 			continue
-// 		}
-
-// 		writer, err := writer(fmt.Sprintf("appsrc format=time name=source ! application/x-rtp,encoding-name=(string)%s ! %s ! filesink location=output.%s", codec.EncodingName, codec.Depayloader, codec.EncodingName))
-// 		if err != nil {
-// 			t.Errorf("failed to create bin: %v", err)
-// 		}
-
-// 		file, ivfErr := os.Open("input.ivf")
-// 		if ivfErr != nil {
-// 			t.Errorf("failed to open input file: %v", ivfErr)
-// 			continue
-// 		}
-
-// 		ivf, header, ivfErr := ivfreader.NewWith(file)
-// 		if ivfErr != nil {
-// 			t.Errorf("failed to read IVF file: %v", ivfErr)
-// 			continue
-// 		}
-
-// 		packetizer := rtp.NewPacketizer(1200, 96, 0, &codecs.VP8Payloader{}, rtp.NewRandomSequencer(), 90000)
-// 		duration := time.Millisecond * time.Duration((float32(header.TimebaseNumerator)/float32(header.TimebaseDenominator))*1000)
-
-// 		wg.Add(2)
-
-// 		go func() {
-// 			defer wg.Done()
-// 			defer tc.Close()
-// 			ticker := time.NewTicker(duration)
-// 			for range ticker.C {
-// 				frame, _, err := ivf.ParseNextFrame()
-// 				if err == io.EOF {
-// 					return
-// 				}
-
-// 				if err != nil {
-// 					t.Errorf("failed to read IVF frame: %v", err)
-// 				}
-
-// 				for _, p := range packetizer.Packetize(frame, uint32(duration.Seconds()*90000)) {
-// 					if err := tc.WriteRTP(p); err != nil {
-// 						t.Errorf("failed to write sample: %v", err)
-// 					}
-// 				}
-// 			}
-// 		}()
-
-// 		src := transcode.NewSource(writer)
-// 		go func() {
-// 			rtpio.CopyRTP(src, tc)
-// 			wg.Done()
-// 		}()
-// 	}
-// 	wg.Wait()
-// }
-
-func TestTranscodingOggToAny(t *testing.T) {
+func TestTranscodingVideo(t *testing.T) {
 	for mime, codec := range server.SupportedCodecs {
-		if !strings.HasPrefix(mime, "audio") {
+		if mime != webrtc.MimeTypeVP8 {
 			continue
 		}
-		log.Printf("ATTEMPTING %s", mime)
+		if !strings.HasPrefix(mime, "video") {
+			continue
+		}
+
+		log.Printf("playing %s", mime)
 
 		outputCodec := server.DefaultOutputCodecs[mime]
 
 		tc, err := transcode.NewTranscoder(
-			server.DefaultOutputCodecs[webrtc.MimeTypeOpus],
+			server.DefaultOutputCodecs[webrtc.MimeTypeVP8],
 			transcode.ToOutputCodec(outputCodec))
 		if err != nil {
 			t.Errorf("failed to create transcoder: %v", err)
 			continue
 		}
 
-		reader, err := reader(fmt.Sprintf("filesrc location=input.ogg ! oggdemux ! rtpopuspay pt=%d ! appsink name=sink", server.DefaultOutputCodecs[webrtc.MimeTypeOpus].PayloadType))
+		reader, err := reader(fmt.Sprintf("filesrc location=input.ivf ! decodebin ! vp8enc ! rtpvp8pay pt=%d mtu=1200 ! appsink name=sink", server.DefaultOutputCodecs[webrtc.MimeTypeVP8].PayloadType))
 		if err != nil {
 			t.Errorf("failed to create bin: %v", err)
 		}
-		writer, err := writer(fmt.Sprintf("appsrc format=time is-live=true name=source ! application/x-rtp,%s ! %s ! queue ! decodebin ! autoaudiosink", codec.ToCaps(outputCodec), codec.Depayloader))
+		writer, err := writer(fmt.Sprintf("appsrc format=time is-live=true name=source ! application/x-rtp,%s ! %s ! queue ! decodebin ! autovideosink", codec.ToCaps(outputCodec), codec.Depayloader))
 		if err != nil {
 			t.Errorf("failed to create bin: %v", err)
 		}
@@ -158,3 +93,46 @@ func TestTranscodingOggToAny(t *testing.T) {
 		wg.Wait()
 	}
 }
+
+// func TestTranscodingAudio(t *testing.T) {
+// 	for mime, codec := range server.SupportedCodecs {
+// 		if !strings.HasPrefix(mime, "audio") {
+// 			continue
+// 		}
+
+// 		log.Printf("playing %s", mime)
+
+// 		outputCodec := server.DefaultOutputCodecs[mime]
+
+// 		tc, err := transcode.NewTranscoder(
+// 			server.DefaultOutputCodecs[webrtc.MimeTypeOpus],
+// 			transcode.ToOutputCodec(outputCodec))
+// 		if err != nil {
+// 			t.Errorf("failed to create transcoder: %v", err)
+// 			continue
+// 		}
+
+// 		reader, err := reader(fmt.Sprintf("filesrc location=input.ogg ! oggdemux ! rtpopuspay pt=%d mtu=1200 ! appsink name=sink", server.DefaultOutputCodecs[webrtc.MimeTypeOpus].PayloadType))
+// 		if err != nil {
+// 			t.Errorf("failed to create bin: %v", err)
+// 		}
+// 		writer, err := writer(fmt.Sprintf("appsrc format=time is-live=true name=source ! application/x-rtp,%s ! %s ! queue ! decodebin ! audioconvert ! pulsesink provide-clock=false", codec.ToCaps(outputCodec), codec.Depayloader))
+// 		if err != nil {
+// 			t.Errorf("failed to create bin: %v", err)
+// 		}
+
+// 		var wg sync.WaitGroup
+// 		wg.Add(2)
+// 		go func() {
+// 			rtpio.CopyRTP(tc, reader)
+// 			tc.Close()
+// 			wg.Done()
+// 		}()
+// 		go func() {
+// 			rtpio.CopyRTP(writer, tc)
+// 			writer.Close()
+// 			wg.Done()
+// 		}()
+// 		wg.Wait()
+// 	}
+// }
